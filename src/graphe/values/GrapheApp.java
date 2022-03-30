ebff21e1d5d871e3741f7d8bf19698e3da8866a6
ZEDIUM-Off <matisse.chenavas@gmail.com>
2022-03-30 13:58:27 +0200

    pull and add gui

--- src/graphe/values/GrapheApp.java
+++ src/graphe/values/GrapheApp.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package graphe.values;
+import java.io.*;
 import java.util.Arrays;
+import javax.swing.*;
 
 /**
  *
@@ -13,104 +15,30 @@ public class GrapheApp {
 
     /**
      * @param args the command line arguments
+     * @throws IOException
      */
-    public static void main(String[] args) {
-        
-        int [][] matriceColoration = {
-                                    {0,1,0,0,0,1},
-                                    {1,0,1,0,0,0},
-                                    {0,1,0,1,0,0},
-                                    {0,0,1,0,1,0},
-                                    {0,0,0,1,0,1},
-                                    {1,0,0,0,1,0}
-                                                };
-        int [][] matriceStabExemple = {
-                            {0,1,0,1,0,0},
-                            {1,0,1,0,1,0},
-                            {0,1,0,1,0,1},
-                            {1,0,1,0,0,0},
-                            {0,1,0,0,0,1},
-                            {0,0,1,0,1,0}
-                           };
-        int [][] tab1  = {
-                            {0,1,1,1,1,0,0,0},
-                            {1,0,1,1,0,1,0,0},
-                            {1,1,0,0,0,0,0,0},
-                            {1,1,0,0,0,0,0,0},
-                            {1,0,0,0,0,1,1,1},
-                            {0,1,0,0,0,1,1,1},
-                            {0,0,0,0,1,1,1,1},
-                            {0,0,0,0,1,1,0,1}
-                            };
-        int [][] cube3d  = {
-                            {0,1,1,0,0,0,1,0},
-                            {1,0,0,1,0,0,0,1},
-                            {1,0,0,1,1,0,0,0},
-                            {0,1,1,0,0,1,0,0},
-                            {0,0,1,0,0,1,1,0},
-                            {0,0,0,1,1,0,0,1},
-                            {1,0,0,0,1,0,0,1},
-                            {0,1,0,0,1,1,0,0}
-                            };
-        int [][] tab = {
-                           {0,1,1,1,0,0},
-                           {1,0,1,1,0,0},
-                           {1,1,0,1,1,1},
-                           {1,1,1,0,1,0},
-                           {0,0,1,1,0,1},
-                           {0,0,1,0,1,0}
-                          };
-        int [][] tab2 = {
-                           {0,1,0,0,0,1},
-                           {1,0,1,0,0,0},
-                           {0,1,0,1,0,0},
-                           {0,0,1,0,1,0},
-                           {0,0,0,1,0,1},
-                           {1,0,0,0,1,0}
-                          };
-        
-        int [][] matriceMultiplication = {
-            {0,1,1,0,0},
-            {0,0,1,0,1},
-            {0,0,0,1,1},
-            {0,1,1,0,1},
-            {1,1,1,0,0}
-        };
-        int [][] matriceMultiplication2 = {
-            {0,1,1},
-            {1,0,1},
-            {1,1,0}
-        };
-        Matrice matrice = new Matrice(matriceColoration);
-        // Matrice matrice2 = new Matrice(matriceMultiplication2);
-        Graphe g1 = new Graphe(matrice);
-        //System.out.println(g1.cheminMinim(2,1));
-        System.out.print(g1);
-        // Graphe g2 = new Graphe(matrice2);
-        // System.out.println(g1.contient(g1));
-        // System.out.println(g1.contient(g2));
-        // System.out.println(g2.contient(g1));
-        // Matrice matrice2 = matrice.selfMultMat();
-        // Matrice matrice3 = matrice.powMat(3);
-        // matrice.afficher();
-        // matrice2.afficher();
-        // matrice3.afficher();
-        // System.out.println(Graphe.K33.taille());
-        // System.out.println(g1.estPlanaire());
-        System.out.println(Arrays.toString(Graphe.versComplet(5).encadrementChromatique()));
-        
-        
-    }
-    
-    public static void afficherListe(int[] tab){
-        System.out.print("[");
-        for(int i = 0; i < tab.length; i++){
-            System.out.print(tab[i]);
-            if (i + 1 != tab.length){
-                System.out.print(", ");
+    public static void main(String[] args) throws IOException {
+        //open input.txt file
+        // File f = new File("."+File.separator+"input.txt");
+        // System.out.println(f.getAbsolutePath());
+        // System.out.println(f.getCanonicalFile().getAbsolutePath());
+        try{
+            FileInputStream fstream = new FileInputStream("input.txt");
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            //read file line by line
+            while ((strLine = br.readLine()) != null)   {
+                //print the content on the console
+                System.out.println (strLine);
             }
+            //close the input stream
+            in.close();
         }
-        System.out.println("]");
+        catch (Exception e){//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+
     }
     
 }
